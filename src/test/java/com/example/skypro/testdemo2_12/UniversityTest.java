14920c215558d0fac0c3c2ef8b1d3b95c67a9e5f
vadim <vadimpoldolin19@gmail.com>
2023-07-07 22:46:06 +0200

    Ошибка

--- src/test/java/com/example/skypro/testdemo2_12/UniversityTest.java
+++ src/test/java/com/example/skypro/testdemo2_12/UniversityTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UniversityTest {
     @Test
@@ -26,6 +26,33 @@ public class UniversityTest {
         actual.add(student1);
         actual.add(student1);
 
-        assertEquals(expected,actual);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getAllStudentsNotNull() {
+        University university = new University();
+        List<Student> expected = university.getAllStudents();
+        assertNotNull(expected);
+    }
+    @Test
+    public void getAllStudentsIsMale() {
+        Student student1 = new Student("Igor", 23, true);
+        Student student2 = new Student("Olga", 18, false);
+        Student student3 = new Student("Kevin", 25, true);
+
+        University university = new University();
+        university.addStudent(student1);
+        university.addStudent(student2);
+        university.addStudent(student3);
+
+        List<Student> expected = university.getAllStudents(true);
+
+        List<Student> actual = new ArrayList<Student>();
+        actual.add(student1);
+        actual.add(student1);
+        actual.add(student1);
+
+        assertEquals(expected, actual);
     }
 }
