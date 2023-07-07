064fdba92fa3737b37eb35a8d93e164cb3ad6ddb
vadim <vadimpoldolin19@gmail.com>
2023-07-07 19:04:29 +0200

    Ошибка

---
+++ src/main/java/com/example/skypro/testdemo2_12/University.java
@@ -0,0 +1,34 @@
+package com.example.skypro.testdemo2_12;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class University {
+    private Map<Integer, Student> allStudents;
+    private int countId = 1;
+
+    public void addStudent(Student student) {
+        if (allStudents == null){
+            allStudents = new HashMap<Integer, Student>();
+        }
+        student.setId(countId);
+        allStudents.put(countId,student);
+        countId++;
+    }
+    public List<Student> getAllStudents(){
+        return new ArrayList<Student>(allStudents.values());
+    }
+    public List<Student> getAllStudents(boolean isMale){
+        List<Student> listAllStudent = new ArrayList<>();
+        for (Student student : allStudents.values()){
+            if (student.isMale() == isMale){
+                listAllStudent.add(student);
+            }
+        }
+        return listAllStudent;
+    }
+
+
+}
---
+++ src/test/java/com/example/skypro/testdemo2_12/UniversityTest.java
@@ -0,0 +1,31 @@
+package com.example.skypro.testdemo2_12;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class UniversityTest {
+    @Test
+    public void getAllStudents() {
+        Student student1 = new Student("Igor", 23, true);
+        Student student2 = new Student("Olga", 18, false);
+        Student student3 = new Student("Kevin", 25, true);
+
+        University university = new University();
+        university.addStudent(student1);
+        university.addStudent(student2);
+        university.addStudent(student3);
+
+        List<Student> expected = university.getAllStudents();
+
+        List<Student> actual = new ArrayList<Student>();
+        actual.add(student1);
+        actual.add(student1);
+        actual.add(student1);
+
+        assertEquals(expected,actual);
+    }
+}
